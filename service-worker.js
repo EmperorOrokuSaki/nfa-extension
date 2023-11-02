6f2a1888fce34ce1fac53a664bf41ca215003e4f
EmperorOrokuSaki <artie.eth@gmail.com>
2023-11-02 20:16:13 +0400

    feat: notifications + new tab logic / don't run, it'll explode your computer (endless new tab opening loop)

--- service-worker.js
+++ service-worker.js
@@ -1,10 +1,39 @@
-chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
+chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
+  chrome.notifications.create('notificationID', {
+    type: 'basic',
+    iconUrl: './images/icon-16.png',
+    title: 'New URL detected',
+    message: 'URL was changed to: ' + tab.url,
+    priority: 2,
+    buttons: [{
+      title: "Open the safe version",
+      // iconUrl: "./images/icon-16.png"
+    }]
+  });
+
   if (changeInfo.status == "complete") console.log("onUpdated:" + tab.url);
 });
 
+chrome.notifications.onButtonClicked.addListener(function (notifId, btnIdx) {
+
+  chrome.tabs.create({
+    url: 'https://ipfs.io/ipfs/bafybeiga3woa76vm6q3siktgpgxyxj2qe53tqtknjpahtpcwncanrujhkq',
+    selected: true,
+  })
+
+  // if (notifId === myNotificationID) {
+  //   if (btnIdx === 0) {
+  //     window.open("...");
+  //   } else if (btnIdx === 1) {
+  //     saySorry();
+  //   }
+  // }
+});
+
+// We may not need the `onActivated` listener as any URL change will trigger the `onUpdated` listener.
 chrome.tabs.onActivated.addListener(() => {
-  chrome.tabs.query({currentWindow: true, active: true}, function(tabs){
-      var currentURL = tabs[0].url;
-      console.log("onActivated:" + currentURL); 
+  chrome.tabs.query({ currentWindow: true, active: true }, function (tabs) {
+    var currentURL = tabs[0].url;
+    console.log("onActivated:" + currentURL);
   })
 });
