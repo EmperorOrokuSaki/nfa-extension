ee5eda32250014dd21a2dfb2b1f0f468f8890d3f
EmperorOrokuSaki <artie.eth@gmail.com>
2023-11-06 16:08:25 +0400

    feat: make the background service worker a module in the manifest. load utils

--- service-worker.js
+++ service-worker.js
@@ -1,20 +1,39 @@
+import { initPrisma, prisma } from './util/prismaHelper.js';
+//importScripts('util/prismaHelper.js');
+
+
 chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
-  chrome.notifications.create('notificationID', {
-    type: 'basic',
-    iconUrl: './images/icon-16.png',
-    title: 'New URL detected',
-    message: 'URL was changed to: ' + tab.url,
-    priority: 2,
-    buttons: [{
-      title: "Open the safe version",
-      // iconUrl: "./images/icon-16.png"
-    }]
+  let url = new URL(tab.url);
+  if (changeInfo.status == "complete") console.log("onUpdated:" + url);
+  if (url.protocol == "ipfs://") return;
+
+  initPrisma();
+
+  let urlLookup = prisma.tokens.findMany({
+    where: {
+      domain: url.hostname.replace('www', '')
+    }
   });
 
-  if (changeInfo.status == "complete") console.log("onUpdated:" + tab.url);
+  if (urlLookup.length > 0) {
+    chrome.notifications.create('notificationID', {
+      type: 'basic',
+      iconUrl: './images/icon-16.png',
+      title: 'This domain is associated with an NFA.',
+      message: 'Do you want to open the safe version?',
+      priority: 2,
+      buttons: [{
+        title: "Yes",
+        // iconUrl: "./images/icon-16.png"
+      }]
+    });
+  }
+
+
+  
 });
 
-chrome.notifications.onButtonClicked.addListener(function (notifId, btnIdx) {
+chrome.notifications.onClicked.addListener((notificationID) => {
 
   chrome.tabs.create({
     url: 'https://ipfs.io/ipfs/bafybeiga3woa76vm6q3siktgpgxyxj2qe53tqtknjpahtpcwncanrujhkq',
@@ -31,9 +50,9 @@ chrome.notifications.onButtonClicked.addListener(function (notifId, btnIdx) {
 });
 
 // We may not need the `onActivated` listener as any URL change will trigger the `onUpdated` listener.
-chrome.tabs.onActivated.addListener(() => {
-  chrome.tabs.query({ currentWindow: true, active: true }, function (tabs) {
-    var currentURL = tabs[0].url;
-    console.log("onActivated:" + currentURL);
-  })
-});
+// chrome.tabs.onActivated.addListener(() => {
+//   chrome.tabs.query({ currentWindow: true, active: true }, function (tabs) {
+//     var currentURL = tabs[0].url;
+//     console.log("onActivated:" + currentURL);
+//   })
+// });
